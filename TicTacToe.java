177cd8aa3a5cc65b70f574ca93b25c6acae8f560
SW-GAGAN <srgagan73@gmail.com>
2021-07-22 15:46:48 +0530

    Adding: TicTacToe game empty board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,19 +1,26 @@
-import java.util.*;
+
+import java.util.Scanner;
+
+/**
+ * print welcome message
+ *
+ * create a empty tic tac toe toe board
+ * board should contain 3 Rows And 3 Calum
+ */
 
 public class TicTacToe {
-    // creating meathod called Creatboard
-    public static char[] CreateBoard() {
-        char[] board = new char[10];
-        for (int i = 1; i < 10; i++) {
-            board[i] = ' ';
-        }
-        return board;
-    }
-
     public static void main(String[] args) {
-        // print welcome message
-        System.out.println("welcome toTicTacTo game");
-        char[] board = CreateBoard();
+        System.out.println("WELCOME TO TIC TAC TOE GAME");
+        Scanner scanner = new Scanner(System.in);
+
+        char[][] gameBoard = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+
+        System.out.println(gameBoard[0][0] + "|" + gameBoard[0][1] + "|" + gameBoard[0][2]);
+        System.out.println("-+-+-");
+        System.out.println(gameBoard[1][0] + "|" + gameBoard[1][1] + "|" + gameBoard[1][2]);
+        System.out.println("-+-+-");
+        System.out.println(gameBoard[2][0] + "|" + gameBoard[2][1] + "|" + gameBoard[2][2]);
 
     }
+
 }
